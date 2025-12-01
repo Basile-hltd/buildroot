df505410503c8dce55d9e934f7a4c00bf6cd8115
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2025-12-01 12:46:21 +0100

    patch et boot.scr

--- board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
 
-install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
+mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd $BINARIES_DIR/boot.scr
