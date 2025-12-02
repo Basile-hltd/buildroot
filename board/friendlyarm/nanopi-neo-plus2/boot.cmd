4ad9988f3765090512944e11f887a77c58608390
Basile Hiltpold <basile@ludis-software.ch>
2025-12-02 14:32:55 +0100

    Add kernel_fdt

--- board/friendlyarm/nanopi-neo-plus2/boot.cmd
+++ board/friendlyarm/nanopi-neo-plus2/boot.cmd
@@ -1,6 +1,6 @@
 setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
-fatload mmc 0 $kernel_addr_r Image
-fatload mmc 0 $fdt_addr_r sun50i-h5-nanopi-neo-plus2.dtb
+fatload mmc 0 0x50000000 kernel_fdt.itb
 
-booti $kernel_addr_r - $fdt_addr_r
+bootm 0x50000000
+
--- board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -2,3 +2,4 @@
 BOARD_DIR="$(dirname $0)"
 
 mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd $BINARIES_DIR/boot.scr
+mkimage -f $BOARD_DIR/kernel_fdt.its -E $BINARIES_DIR/kernel_fdt.itb
