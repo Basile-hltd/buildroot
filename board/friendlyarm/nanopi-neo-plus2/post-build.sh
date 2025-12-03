4c475849340d94720d7824f73ba7d5af8081f258
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2025-12-03 13:05:49 +0100

    patch postbuild for ext4

--- board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -4,13 +4,10 @@ BOARD_DIR="$(dirname $0)"
 mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd $BINARIES_DIR/boot.scr
 mkimage -f $BOARD_DIR/kernel_fdt.its -E $BINARIES_DIR/kernel_fdt.itb
 
-dd if=/dev/zero of=$BOARD_DIR/boot.img bs=1M count=64
-mkfs.ext4 boot.img
-mkdir $BOARD_DIR/tmp/mnt
-mount -o loop boot.img $BOARD_DIR/tmp/mnt
+rm -rf $BOARD_DIR/boot_ext4_dir
+mkdir $BOARD_DIR/boot_ext4_dir
+cp $BOARD_DIR/kernel_fdt.itb $BOARD_DIR/boot_ext4_dir/
+cp $BOARD_DIR/boot.scr $BOARD_DIR/boot_ext4_dir/
 
-cp $BINARIES_DIR/kernel_fdt.itb $BOARD_DIR/tmp/mnt
-cp $BINARIES_DIR/boot.scr $BOARD_DIR/tmp/mnt
-
-umount $BOARD_DIR/tmp/mnt
-mv "$BOARD_DIR/boot.img" "$BINARIES_DIR/boot.ext4"
+dd if=/dev/zero of=$BINARIES_DIR/boot.ext4 bs=1M count=64
+mkfs.ext4 -d $BOARD_DIR/boot_ext4_dir -L boot -m 0 $BINARIES_DIR/boot.ext4
