0dac964f7a3fd753c6712453c13c989fd886cb0f
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2025-12-04 16:33:55 +0100

    patch uboot extras config and post-build

--- board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -4,10 +4,10 @@ BOARD_DIR="$(dirname $0)"
 mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd $BINARIES_DIR/boot.scr
 mkimage -f $BOARD_DIR/kernel_fdt.its -E $BINARIES_DIR/kernel_fdt.itb
 
-rm -rf $BOARD_DIR/boot_ext4_dir
-mkdir $BOARD_DIR/boot_ext4_dir
-cp $BOARD_DIR/kernel_fdt.itb $BOARD_DIR/boot_ext4_dir/
-cp $BOARD_DIR/boot.scr $BOARD_DIR/boot_ext4_dir/
+rm -rf $BINARIES_DIR/boot_ext4_dir
+mkdir $BINARIES_DIR/boot_ext4_dir
+cp $BINARIES_DIR/kernel_fdt.itb $BINARIES_DIR/boot_ext4_dir/
+cp $BINARIES_DIR/boot.scr $BINARIES_DIR/boot_ext4_dir/
 
 dd if=/dev/zero of=$BINARIES_DIR/boot.ext4 bs=1M count=64
-mkfs.ext4 -d $BOARD_DIR/boot_ext4_dir -L boot -m 0 $BINARIES_DIR/boot.ext4
+mkfs.ext4 -d $BINARIES_DIR/boot_ext4_dir -L boot -m 0 $BINARIES_DIR/boot.ext4
