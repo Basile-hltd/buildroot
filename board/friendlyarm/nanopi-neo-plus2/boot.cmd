5be4cdf8077f3094d7f0c49c4fb975b8bfccdce4
Basile Hiltpold <basile@ludis-software.ch>
2025-12-09 13:38:18 +0100

    Patch load addr

--- board/friendlyarm/nanopi-neo-plus2/boot.cmd
+++ board/friendlyarm/nanopi-neo-plus2/boot.cmd
@@ -1,6 +1,6 @@
 setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
-ext4load mmc 0 0x40080000 kernel_fdt.itb
+ext4load mmc 0 0x50000000 kernel_fdt.itb
 
-bootm 0x40080000
+bootm 0x50000000
 
